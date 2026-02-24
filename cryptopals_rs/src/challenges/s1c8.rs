6b4ac0c3951bf49b7edcfa247f5335fdd3d5ca32
hakansabol <hakansabol@gmail.com>
2026-02-24 13:01:01 -0800

    s1c8

--- cryptopals_rs/src/challenges/s1c8.rs
+++ cryptopals_rs/src/challenges/s1c8.rs
@@ -1,25 +1,24 @@
-use std::{
-    collections::HashMap,
-    thread::{self, JoinHandle},
-};
+use std::collections::HashMap;
 
-use cryptopals_rs::{Number, decrypt_aes_128_ecb};
+use cryptopals_rs::Number;
 
 fn main() {
     let strs: Vec<&str> = include_str!("s1c8.txt").lines().collect();
 
+    let mut d = HashMap::new();
     for i in 0..204 {
         let s = strs[i].to_owned();
-        thread::spawn(move || {
-            let mut occurences: HashMap<&str, i32> = HashMap::new();
-            let dat = Number::from_hex(&s).to_bytes();
-            for i in 0..dat.len() / 16 {
-                let c: &[u8] = &dat[i * 16..(i + 1) * 16];
-                let ms = Number::from_bytes(c).to_string();
-                occurences[]
-            }
-
-            println!("{s}");
-        });
+        let n = Number::from_string(&s);
+        let bytelen = n.bit_len() / 8;
+        for j in 0..bytelen/16 {
+            let block = &s.bytes().collect::<Vec<_>>()[j*16..(j+1)*16];
+            let tst = Number::from_bytes(block).to_string();
+            *(d.entry(tst).or_insert(0)) += 1;
+        }
+    }
+    for (a,b) in d {
+        if b > 1 {
+            println!("{a}: {b}");
+        }
     }
 }
