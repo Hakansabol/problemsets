ebe2d3c1817938df8a3a9024cc81c8d3de0aafe0
hakansabol <hakansabol@gmail.com>
2026-03-09 01:33:45 -0700

    encrypt decrypt

--- cryptopals_rs/src/ciphers.rs
+++ cryptopals_rs/src/ciphers.rs
@@ -1,6 +1,4 @@
-use openssl::{
-    symm::{Cipher, decrypt},
-};
+use openssl::symm::{Cipher, decrypt, encrypt};
 
 use crate::Number;
 
@@ -14,9 +12,44 @@ pub fn decrypt_aes_128_ecb(data: Number, key: Number) -> Number {
         None,
         &data.to_bytes(),
     )
-
     .expect("=bad cipher");
-    let out =
-        Number::from_bytes(&dec);
+    let out = Number::from_bytes(&dec);
     out
+}
+
+pub fn encrypt_aes_128_ecb(data: Number, key: Number, iv: &[u8]) -> Number {
+    assert_eq!(data.bit_len() % 128, 0, "=wrong ciphertext bitlength");
+    assert_eq!(key.bit_len() % 128, 0, "=wrong key bitlength");
+
+    let dec = encrypt(
+        Cipher::aes_128_ecb(),
+        &key.to_bytes(),
+        Some(iv),
+        &data.to_bytes(),
+    )
+    .expect("=bad cipher");
+    let out = Number::from_bytes(&dec);
+    out
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{Number, ciphers::encrypt_aes_128_ecb, decrypt_aes_128_ecb};
+
+    #[test]
+    fn decrypt_encrypted() {
+        let data = Number::from_string("YELLOW SUBMARINE");
+        let iv = Number::from_string("");
+        iv.pad(16);
+        let enc = encrypt_aes_128_ecb(
+            data,
+            Number::from_bytes(b"YELLOW SUBMARING"),
+            &iv.to_bytes(),
+        );
+        let dec = decrypt_aes_128_ecb(enc, Number::from_bytes(b"YELLOW SUBMARING"));
+        assert_eq!(
+            dec.to_bytes(),
+            Number::from_string("YELLOW SUBMARINE").to_bytes()
+        );
+    }
 }
--- cryptopals_rs/tests/s2c9.rs
+++ cryptopals_rs/tests/s2c9.rs
@@ -6,7 +6,7 @@ mod tests {
     fn pkcs_padding() {
         let input = Number::from_string("YELLOW SUBMARINE");
         let output = input.pad(20);
-        assert_eq!(output.bit_len(), 20*8);
+        assert_eq!(output.bit_len(), 20 * 8);
         assert_eq!(output.to_string(), "YELLOW SUBMARINE\x04\x04\x04\x04");
     }
 }
