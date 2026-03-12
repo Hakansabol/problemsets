2abe70c461100608d7dff8110742e928ccf0ecdd
hakansabol <hakansabol@gmail.com>
2026-03-11 17:56:39 -0700

    cses 1-6

--- cses/cses_rs/src/weird_algorithm.rs
+++ cses/cses_rs/src/weird_algorithm.rs
@@ -1,4 +1,61 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-	Ok(())
+/*
+This template is made by Naman Garg <naman.rustp@gmail.com>
+GitHub : https://github.com/namanlp
+GitLab : https://gitlab.com/namanlp
+Website : https://rustp.org
+
+You can visit https://rustp.org/basic-programs/basic-template/
+for understanding the template
+
+Feel free to copy the template, but not the solutions :D
+Thank You
+ */
+
+#![allow(unused)]
+
+use std::io::stdin;
+
+fn take_int() -> usize {
+    let mut input = String::new();
+    stdin().read_line(&mut input).unwrap();
+    return input.trim().parse().unwrap();
 }
 
+fn take_vector() -> Vec<usize> {
+    let mut input = String::new();
+    stdin().read_line(&mut input).unwrap();
+    let arr: Vec<usize> = input
+        .trim()
+        .split_whitespace()
+        .map(|x| x.parse().unwrap())
+        .collect();
+    return arr;
+}
+
+fn take_string() -> Vec<char> {
+    let mut input = String::new();
+    stdin().read_line(&mut input).unwrap();
+    let vec: Vec<char> = input.trim().chars().collect();
+    return vec;
+}
+fn to_string(vec: Vec<char>) -> String {
+    return vec.iter().collect::<String>();
+}
+
+fn solve() {
+    let mut n = take_int();
+
+    let mut ans = vec![];
+    while n > 1 {
+        ans.push(n);
+        n = if n % 2 == 1 { n * 3 + 1 } else { n / 2 }
+    }
+    ans.push(1);
+
+    let out = ans.iter().map(|x| x.to_string()).collect::<Vec<String>>().join(" ");
+    println!("{out}");
+}
+
+pub fn main() {
+    solve();
+}
